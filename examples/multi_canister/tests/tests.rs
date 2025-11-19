2ddb6293d885b76b7fafd21a2c430cef96abbaf7
Louis Pahlavi <louis.pahlavi@dfinity.org>
2025-11-19 10:53:24 +0100

    DEFI-2485: Use `PocketIcRuntime` in integration tests

--- examples/multi_canister/tests/tests.rs
+++ examples/multi_canister/tests/tests.rs
@@ -1,17 +1,15 @@
-use candid::Principal;
 use test_fixtures::Setup;
 use uuid::Uuid;
 
-#[test]
-fn should_make_parallel_http_requests() {
-    let setup = Setup::new("multi_canister");
-    let http_canister = setup.canister();
+#[tokio::test]
+async fn should_make_parallel_http_requests() {
+    let setup = Setup::new("multi_canister").await;
 
-    let http_request_results = http_canister.update_call::<_, Vec<String>>(
-        Principal::anonymous(),
-        "make_parallel_http_requests",
-        (),
-    );
+    let http_request_results = setup
+        .canister()
+        .await
+        .update_call::<_, Vec<String>>("make_parallel_http_requests", ())
+        .await;
 
     for uuid in http_request_results {
         assert!(Uuid::parse_str(uuid.as_str()).is_ok());
