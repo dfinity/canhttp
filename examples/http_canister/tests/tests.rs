db87455f548fe6c75d59d2b1fdd2682e5fefd459
Louis Pahlavi <louis.pahlavi@dfinity.org>
2025-11-24 11:18:56 +0100

    test: use `PocketIcRuntime` in integration tests (#50)
    (DEFI-2485) Use the `PocketIcRuntime` in the test fixtures for the
    integration tests.

--- examples/http_canister/tests/tests.rs
+++ examples/http_canister/tests/tests.rs
@@ -1,16 +1,13 @@
-use candid::Principal;
 use test_fixtures::Setup;
 
-#[test]
-fn should_make_http_post_request() {
-    let setup = Setup::new("http_canister");
-    let http_canister = setup.canister();
+#[tokio::test]
+async fn should_make_http_post_request() {
+    let setup = Setup::new("http_canister").await;
 
-    let http_request_result = http_canister.update_call::<_, String>(
-        Principal::anonymous(),
-        "make_http_post_request",
-        (),
-    );
+    let http_request_result = setup
+        .canister()
+        .update_call::<_, String>("make_http_post_request", ())
+        .await;
 
     assert!(http_request_result.contains("Hello, World!"));
     assert!(http_request_result.contains("\"X-Id\": \"42\""));
