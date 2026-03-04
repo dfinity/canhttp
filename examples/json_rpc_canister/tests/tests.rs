8ae964ca734b2f561b9ddc883bd29b70f7b9c74d
Louis Pahlavi <louis.pahlavi@dfinity.org>
2026-03-04 11:45:11 +0100

    Relax assertion

--- examples/json_rpc_canister/tests/tests.rs
+++ examples/json_rpc_canister/tests/tests.rs
@@ -1,9 +1,5 @@
 use candid::{CandidType, Deserialize};
-use regex_lite::Regex;
 use test_fixtures::Setup;
-
-/// See https://solana.com/developers/guides/advanced/exchange#basic-verification
-static SOLANA_PUBKEY_REGEX: &str = r"^[1-9A-HJ-NP-Za-km-z]{32,44}$";
 
 #[tokio::test]
 async fn should_make_json_rpc_request() {
@@ -27,9 +23,7 @@ async fn should_make_batch_json_rpc_request() {
         .await;
 
     assert!(result.slot > 0);
-    assert!(Regex::new(SOLANA_PUBKEY_REGEX)
-        .unwrap()
-        .is_match(&result.leader));
+    assert!(result.leader.len() > 0);
 }
 
 #[derive(CandidType, Deserialize)]
