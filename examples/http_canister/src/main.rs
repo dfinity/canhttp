7bc6ea7efd44a199a1c15a6c4005fff9b7c704fb
Louis Pahlavi <louis.pahlavi@dfinity.org>
2026-03-06 16:30:44 +0100

    Make examples simpler

--- examples/http_canister/src/main.rs
+++ examples/http_canister/src/main.rs
@@ -10,11 +10,14 @@ use http_canister::InsufficientCyclesError;
 use ic_cdk::update;
 use tower::{BoxError, Service, ServiceBuilder, ServiceExt};
 
-/// Make an HTTP POST request and do not charge the user for it.
+/// Make an HTTP POST request.
 #[update]
-pub async fn charge_canister_for_http_request() -> String {
-    // Use cycles from the canister to pay for HTTPs outcalls
-    let response = make_http_post_request(ChargeMyself::default())
+pub async fn make_http_post_request() -> String {
+    let response = http_client(ChargeMyself::default())
+        .ready()
+        .await
+        .expect("Client should be ready")
+        .call(request())
         .await
         .expect("Request should succeed");
 
@@ -23,32 +26,25 @@ pub async fn charge_canister_for_http_request() -> String {
     String::from_utf8_lossy(response.body()).to_string()
 }
 
-/// Make an HTTP POST request and charge the user for it.
+/// Make an HTTP POST request and charge the user cycles for it.
 #[update]
-pub async fn charge_caller_for_http_request() -> Result<String, InsufficientCyclesError> {
+pub async fn make_http_post_request_and_charge_user_cycles(
+) -> Result<String, InsufficientCyclesError> {
     // Use cycles attached by the caller to pay for HTTPs outcalls and charge an additional fee.
-    match make_http_post_request(ChargeCaller::new(|_request, cost| cost + 1_000_000)).await {
-        Ok(response) => {
-            assert_eq!(response.status(), http::StatusCode::OK);
-            Ok(String::from_utf8_lossy(response.body()).to_string())
-        }
-        Err(e) => Err(InsufficientCyclesError::try_from(e).expect("Request should succeed")),
-    }
-}
-
-async fn make_http_post_request<C>(
-    cycles_charging_policy: C,
-) -> Result<http::Response<Vec<u8>>, BoxError>
-where
-    C: CyclesChargingPolicy + Clone,
-    <C as CyclesChargingPolicy>::Error: std::error::Error + Send + Sync + 'static,
-{
-    http_client(cycles_charging_policy)
+    match http_client(ChargeCaller::new(|_request, cost| cost + 1_000_000))
         .ready()
         .await
         .expect("Client should be ready")
         .call(request())
         .await
+    {
+        Ok(response) => {
+            assert_eq!(response.status(), http::StatusCode::OK);
+            Ok(String::from_utf8_lossy(response.body()).to_string())
+        }
+        // Return an error if the call failed due to insufficient cycles, otherwise panic.
+        Err(e) => Err(InsufficientCyclesError::try_from(e).expect("Request should succeed")),
+    }
 }
 
 /// Make multiple HTTP POST requests in a loop,
